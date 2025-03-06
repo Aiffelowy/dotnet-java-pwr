adbf0acfd228f5abf15654730647c9ec8dc305c9
adask <adask@DESKTOP-L2LGPO4>
2025-03-06 17:00:40 +0100

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿//mail [.NET][CZW 17:05][NR tematu]
+//trzeba zrobic dokumentacje kodu
+using fizzbuzz;
+
+
+class MyApp
+{
+    static void Main(string[] args) {
+        var fizzbuzz = new FizzBuzz(100);
+        fizzbuzz.run();
+    }
+}

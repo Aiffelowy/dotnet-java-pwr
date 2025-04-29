056a8e3e7900fa2d162c3c0f8e172775ca8c361b
Aiffelowy <adas.krzywicki2@gmail.com>
2025-04-29 17:53:23 +0200

    rewritten tests

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,260 +1,301 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Drawing;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using Errors;
 using Testing;
 
-class Skibid
-{
-    public static Timed test_parallel(Random rng)
-    {
-        var matrix1 = Matrix.random(rng, 100, 100);
-        var matrix2 = Matrix.random(rng, 100, 100);
-        return Timed.time(() =>
-        {
-            matrix1.mult_parallel(matrix2, 8);
-        });
-    }
-    public static Timed test_single(Random rng)
-    {
-        var matrix1 = Matrix.random(rng, 1000, 1000);
-        var matrix2 = Matrix.random(rng, 1000, 1000);
-        return Timed.time(() =>
-        {
-            matrix1.mult(matrix2);
-        });
-    }
-    public static Timed test_threads(Random rng)
-    {
-        var matrix1 = Matrix.random(rng, 100, 100);
-        var matrix2 = Matrix.random(rng, 100, 100);
-        return Timed.time(() =>
-        {
-            matrix1.mult_threads(matrix2, 8);
-        });
-    }
+// WAY TO LAZY to rewrite tests that have multiple custom parameters
+class Skibidi {
+  public static Timed test_single(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult(matrix2); });
+  }
+  public static Timed test_parallel2(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_parallel(matrix2, 2); });
+  }
+
+  public static Timed test_parallel4(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_parallel(matrix2, 4); });
+  }
+
+  public static Timed test_parallel8(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_parallel(matrix2, 8); });
+  }
+  public static Timed test_parallel16(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_parallel(matrix2, 16); });
+  }
+  public static Timed test_threads2(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_threads(matrix2, 2); });
+  }
+
+  public static Timed test_threads4(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_threads(matrix2, 4); });
+  }
+  public static Timed test_threads8(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_threads(matrix2, 8); });
+  }
+  public static Timed test_threads16(Random rng, int data_size) {
+    var matrix1 = Matrix.random(rng, data_size, data_size);
+    var matrix2 = Matrix.random(rng, data_size, data_size);
+    return Timed.time(() => { matrix1.mult_threads(matrix2, 16); });
+  }
 }
 
-class Matrix
-{
-    private int[,] data;
-    private Matrix(int[,] data) { this.data = data; }
+class Matrix {
+  private int[,] data;
+  private Matrix(int[,] data) { this.data = data; }
 
-    public static Matrix from(int[,] matrix)
-    {
-        return new Matrix(matrix);
-    }
+  public static Matrix from(int[,] matrix) { return new Matrix(matrix); }
 
+  private static (int, int) calc_block_size(int rows, int cols, int threads) {
+    int elements = rows * cols;
 
-    private static (int, int) calc_block_size(int rows, int cols, int threads)
-    {
-        int elements = rows * cols;
+    if (elements <= 64 || rows < 9 || cols < 9)
+      return (rows, cols);
 
-        if (elements <= 64 || rows < 9 || cols < 9)
-            return (rows, cols);
+    int blocks = threads * 4;
+    int area = Math.Max(1, elements / blocks);
+    double aspect_ratio = (double)rows / cols;
+    double sqrt_area = Math.Sqrt(area);
 
-        int blocks = threads * 4;
-        int area = Math.Max(1, elements / blocks);
-        double aspect_ratio = (double)rows / cols;
-        double sqrt_area = Math.Sqrt(area);
+    int rows_new = (int)(sqrt_area * Math.Sqrt(aspect_ratio));
+    int cols_new = (int)(sqrt_area / Math.Sqrt(aspect_ratio));
 
-        int rows_new = (int)(sqrt_area * Math.Sqrt(aspect_ratio));
-        int cols_new = (int)(sqrt_area / Math.Sqrt(aspect_ratio));
+    return (Math.Clamp(rows_new, 4, rows), Math.Clamp(cols_new, 4, cols));
+  }
+  private static void mult_block(int[,] A, int[,] B, int[,] result, int row,
+                                 int column, int block_rows, int block_cols) {
+    int rA = A.GetLength(0);
+    int cA = A.GetLength(1);
+    int cB = B.GetLength(1);
 
-        return (Math.Clamp(rows_new, 4, rows), Math.Clamp(cols_new, 4, cols));
-    }
-    private static void mult_block(int[,] A, int[,] B, int[,] result, int row, int column, int block_rows, int block_cols)
-    {
-        int rA = A.GetLength(0);
-        int cA = A.GetLength(1);
-        int cB = B.GetLength(1);
+    int row_end = Math.Min(row + block_rows, rA);
+    int col_end = Math.Min(column + block_cols, cB);
 
-        int row_end = Math.Min(row + block_rows, rA);
-        int col_end = Math.Min(column + block_cols, cB);
-
-        for (int i = row; i < row_end; i++)
-        {
-            for (int j = column; j < col_end; j++)
-            {
-                int sum = 0;
-                for (int k = 0; k < cA; k++)
-                {
-                    sum += A[i, k] * B[k, j];
-                }
-                result[i, j] = sum;
-            }
+    for (int i = row; i < row_end; i++) {
+      for (int j = column; j < col_end; j++) {
+        int sum = 0;
+        for (int k = 0; k < cA; k++) {
+          sum += A[i, k] * B[k, j];
         }
+        result[i, j] = sum;
+      }
+    }
+  }
+
+  public static Matrix random(Random rng, int m, int n) {
+    var matrix = new int[m, n];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i, j] = rng.Next(0, 420);
+      }
     }
 
-    public static Matrix random(Random rng, int m, int n)
-    {
-        var matrix = new int[m, n];
+    return new Matrix(matrix);
+  }
 
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                matrix[i, j] = rng.Next(0, 420);
-            }
+  public Option<Matrix> mult(Matrix other) {
+    var rA = this.data.GetLength(0);
+    var cA = this.data.GetLength(1);
+    var rB = other.data.GetLength(0);
+    var cB = other.data.GetLength(1);
+
+    if (cA != rB)
+      return Option.None<Matrix>();
+
+    var new_matrix = new int[rA, cB];
+
+    for (int x = 0; x < rA; x++) {
+      for (int i = 0; i < cB; i++) {
+        var temp = 0;
+        for (int j = 0; j < cA; j++) {
+          temp += this.data[x, j] * other.data[j, i];
         }
-
-        return new Matrix(matrix);
+        new_matrix[x, i] = temp;
+      }
     }
 
-    public Option<Matrix> mult(Matrix other) {
-        var rA = this.data.GetLength(0);
-        var cA = this.data.GetLength(1);
-        var rB = other.data.GetLength(0);
-        var cB = other.data.GetLength(1);
+    return Matrix.from(new_matrix).as_some();
+  }
 
-        if (cA != rB) return Option.None<Matrix>();
+  public Option<Matrix> mult_parallel(Matrix other, int max_threads) {
+    var rA = this.data.GetLength(0);
+    var cA = this.data.GetLength(1);
+    var rB = other.data.GetLength(0);
+    var cB = other.data.GetLength(1);
 
-        var new_matrix = new int[rA, cB];
+    if (cA != rB)
+      return Option.None<Matrix>();
 
-        for(int x = 0; x < rA; x++)
-        {
-            for (int i = 0; i < cB; i++)
-            {
-                var temp = 0;
-                for (int j = 0; j < cA; j++)
-                {
-                    temp += this.data[x, j] * other.data[j, i];
-                }
-                new_matrix[x, i] = temp;
-            }
+    ParallelOptions opts =
+        new ParallelOptions() { MaxDegreeOfParallelism = max_threads };
+    var new_matrix = new int[rA, cB];
+
+    Parallel.For(0, rA, opts, x => {
+      for (int i = 0; i < cB; i++) {
+        var temp = 0;
+        for (int j = 0; j < cA; j++) {
+          temp += this.data[x, j] * other.data[j, i];
         }
+        new_matrix[x, i] = temp;
+      }
+    });
 
-        return Matrix.from(new_matrix).as_some();
+    return Matrix.from(new_matrix).as_some();
+  }
+
+  public Option<Matrix> mult_threads(Matrix other, int max_threads) {
+    var rA = this.data.GetLength(0);
+    var cA = this.data.GetLength(1);
+    var rB = other.data.GetLength(0);
+    var cB = other.data.GetLength(1);
+
+    if (cA != rB)
+      return Option.None<Matrix>();
+
+    var new_matrix = new int[rA, cB];
+
+    var blocks = new List<(int, int)>();
+    var (block_rows, block_cols) = Matrix.calc_block_size(rA, cB, max_threads);
+    for (int i = 0; i < rA; i += block_rows) {
+      for (int j = 0; j < cB; j += block_cols) {
+        blocks.Add((i, j));
+      }
     }
 
-    public Option<Matrix> mult_parallel(Matrix other, int max_threads)
-    {
-        var rA = this.data.GetLength(0);
-        var cA = this.data.GetLength(1);
-        var rB = other.data.GetLength(0);
-        var cB = other.data.GetLength(1);
+    // var thread_queues =
+    //    Enumerable.Range(0, max_threads).Select(queue => new Queue<(int,
+    //    int)>()).ToList();
 
-        if (cA != rB) return Option.None<Matrix>();
+    // for (int i = 0; i < blocks.Count; i++)
+    //{
+    //     thread_queues[i % max_threads].Enqueue(blocks[i]);
+    // }
 
+    var next_block = -1;
 
-        ParallelOptions opts = new ParallelOptions() { MaxDegreeOfParallelism = max_threads };
-        var new_matrix = new int[rA, cB];
+    var threads =
+        Enumerable.Range(0, max_threads)
+            .Select(thread => new Thread(() => {
+                      // var queue = thread_queues[thread];
+                      int index;
+                      // while (queue.TryDequeue(out var block))
+                      while ((index = Interlocked.Increment(ref next_block)) <
+                             blocks.Count) {
+                        var (row, column) = blocks[index];
+                        // var (row, column) = block;
+                        Matrix.mult_block(this.data, other.data, new_matrix,
+                                          row, column, block_rows, block_cols);
+                      }
+                    }))
+            .ToList();
+    threads.ForEach(thread => thread.Start());
+    threads.ForEach(thread => thread.Join());
 
-        Parallel.For(0, rA, opts, x =>
-        {
-            for (int i = 0; i < cB; i++)
-            {
-                var temp = 0;
-                for (int j = 0; j < cA; j++)
-                {
-                    temp += this.data[x, j] * other.data[j, i];
-                }
-                new_matrix[x, i] = temp;
-            }
-        });
+    return Matrix.from(new_matrix).as_some();
+  }
 
-        return Matrix.from(new_matrix).as_some();
+  public override string ToString() {
+    string result = "";
+    int rows = this.data.GetLength(0);
+    int cols = this.data.GetLength(1);
+
+    int[] cols_widths = new int[cols];
+    for (int j = 0; j < cols; j++) {
+      int max_width = 0;
+      for (int i = 0; i < rows; i++) {
+        int len = this.data[i, j].ToString().Length;
+        if (len > max_width)
+          max_width = len;
+      }
+      cols_widths[j] = max_width;
     }
 
-    public Option<Matrix> mult_threads(Matrix other, int max_threads)
-    {
-        var rA = this.data.GetLength(0);
-        var cA = this.data.GetLength(1);
-        var rB = other.data.GetLength(0);
-        var cB = other.data.GetLength(1);
+    for (int i = 0; i < rows; i++) {
+      result += "| ";
+      for (int j = 0; j < cols; j++) {
+        string value = this.data[i, j].ToString();
+        int width = cols_widths[j];
 
-        if (cA != rB) return Option.None<Matrix>();
+        int left_padding = ((width - value.Length) / 2);
+        int right_padding = (width - value.Length - left_padding);
 
-        var new_matrix = new int[rA, cB];
-
-        var blocks = new List<(int, int)>();
-        var (block_rows, block_cols) = Matrix.calc_block_size(rA, cB, max_threads);
-        for (int i = 0; i < rA; i += block_rows)
-        {
-            for (int j = 0; j < cB; j += block_cols)
-            {
-                blocks.Add((i, j));
-            }
-        }
-
-        //var thread_queues =
-        //   Enumerable.Range(0, max_threads).Select(queue => new Queue<(int, int)>()).ToList();
-
-        //for (int i = 0; i < blocks.Count; i++)
-        //{
-        //    thread_queues[i % max_threads].Enqueue(blocks[i]);
-        //}
-
-        var next_block = -1;
-
-        var threads = Enumerable.Range(0, max_threads).Select(thread => new Thread(() =>
-        {
-            //var queue = thread_queues[thread];
-            int index;
-            //while (queue.TryDequeue(out var block))
-            while((index = Interlocked.Increment(ref next_block)) < blocks.Count)
-            {
-                var (row, column) = blocks[index];
-                //var (row, column) = block;
-                Matrix.mult_block(this.data, other.data, new_matrix, row, column, block_rows, block_cols);
-            }
-        })).ToList();
-        threads.ForEach(thread => thread.Start());
-        threads.ForEach(thread => thread.Join());
-
-        return Matrix.from(new_matrix).as_some();
+        result += new string(' ', left_padding);
+        result += value;
+        result += new string(' ', right_padding) + ' ';
+      }
+      result += "|\n";
     }
 
-    public override string ToString()
-    {
-        string result = "";
-        int rows = this.data.GetLength(0);
-        int cols = this.data.GetLength(1);
-
-        int[] cols_widths = new int[cols];
-        for (int j = 0; j < cols; j++)
-        {
-            int max_width = 0;
-            for (int i = 0; i < rows; i++)
-            {
-                int len = this.data[i, j].ToString().Length;
-                if (len > max_width) max_width = len;
-            }
-            cols_widths[j] = max_width;
-        }
-
-        for (int i = 0; i < rows; i++)
-        {
-            result += "| ";
-            for (int j = 0; j < cols; j++)
-            {
-                string value = this.data[i, j].ToString();
-                int width = cols_widths[j];
-
-                int left_padding = ((width - value.Length) / 2);
-                int right_padding = (width - value.Length - left_padding);
-
-                result += new string(' ', left_padding);
-                result += value;
-                result += new string(' ', right_padding) + ' ';
-            }
-            result += "|\n";
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
 
+class MyApp {
+  public static void Main(string[] args) {
+    var suite2 =
+        TestSuite.New()
+            .set_repeats(6)
+            .data_sizes(new int[] { 50, 100, 150, 250, 500, 1000, 2000 })
+            .add_test(Test.New("normal", Skibidi.test_single))
+            .add_test(Test.New("parallel", Skibidi.test_parallel2))
+            .add_test(Test.New("threads", Skibidi.test_threads2));
 
-class MyApp
-{
-    public static void Main(string[] args)
-    {
-        var test1 = new Test(Skibid.test_parallel, "parallel", 10);
-        var test2 = new Test(Skibid.test_threads, "threads", 10);
+    var suite4 =
+        TestSuite.New()
+            .set_repeats(6)
+            .data_sizes(new int[] { 50, 100, 150, 250, 500, 1000, 2000 })
+            .add_test(Test.New("parallel", Skibidi.test_parallel4))
+            .add_test(Test.New("threads", Skibidi.test_threads4));
 
-        Console.WriteLine(test1.run());
-        Console.WriteLine(test2.run());
-    }
+    var suite8 =
+        TestSuite.New()
+            .set_repeats(6)
+            .data_sizes(new int[] { 50, 100, 150, 250, 500, 1000, 2000 })
+            .add_test(Test.New("parallel", Skibidi.test_parallel8))
+            .add_test(Test.New("threads", Skibidi.test_threads8));
+
+    var suite16 =
+        TestSuite.New()
+            .set_repeats(6)
+            .data_sizes(new int[] { 50, 100, 150, 250, 500, 1000, 2000 })
+            .add_test(Test.New("parallel", Skibidi.test_parallel16))
+            .add_test(Test.New("threads", Skibidi.test_threads16));
+
+    var results2 = suite2.run();
+    var results4 = suite4.run();
+    var results8 = suite8.run();
+    var results16 = suite16.run();
+
+    Console.WriteLine("TWO THREADS");
+    Console.WriteLine(results2.to_csv());
+
+    Console.WriteLine("FOUR THREADS");
+    Console.WriteLine(results4.to_csv());
+
+    Console.WriteLine("EIGHT THREADS");
+    Console.WriteLine(results8.to_csv());
+
+    Console.WriteLine("SIXTEEN THREADS");
+    Console.WriteLine(results16.to_csv());
+  }
 }
--- Threads/Tests.cs
+++ Threads/Tests.cs
@@ -1,65 +1,124 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
+using Testing;
 
+namespace Testing {
+readonly struct Timed {
+  readonly public long time_taken;
+  private Timed(long time) { this.time_taken = time; }
+  public static Timed time(Action func) {
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    func();
+    watch.Stop();
+    return new Timed(watch.ElapsedMilliseconds);
+  }
+}
 
-namespace Testing
-{
-    class TestResult
-    {
-        long time;
-        string name;
-        int repeats;
+readonly struct TestResult {
+  readonly public long[] times;
+  readonly public string name;
 
-        public TestResult(string name, int repeats, long avg)
-        {
-            this.name = name;
-            this.repeats = repeats;
-            this.time = avg;
+  public TestResult(long[] times, string name) {
+    this.times = times;
+    this.name = name;
+  }
+}
+
+class TestResults {
+  TestResult[] results;
+  readonly public int[] data_sizes;
+
+  public TestResults(TestResult[] results, int[] data_sizes) {
+    this.results = results;
+    this.data_sizes = data_sizes;
+  }
+  public string to_csv() {
+    string csv = "";
+    int s = this.data_sizes.Count();
+    for (int t = -1; t < this.results.Count(); t++) {
+      for (int ds = -1; ds < s; ds++) {
+        if (t == -1) {
+          if (ds == -1) {
+            csv += "sizes:,";
+          } else {
+            csv += this.data_sizes[ds] + ",";
+          }
+          continue;
         }
 
-        public override string ToString()
-        {
-            return $"Test {this.name} ({this.repeats} samples): {this.time}ms average";
+        if (ds == -1) {
+          csv += this.results[t].name + ",";
+          continue;
         }
+
+        csv += this.results[t].times[ds] + ",";
+      }
+      csv += "\n";
+    }
+    return csv;
+  }
+}
+
+readonly struct Test {
+  readonly public string name;
+  readonly Func<Random, int, Timed> test_func;
+
+  private Test(string name, Func<Random, int, Timed> test_func) {
+    this.name = name;
+    this.test_func = test_func;
+  }
+  public static Test New(string name, Func<Random, int, Timed> test_func) {
+    return new Test(name, test_func);
+  }
+  public Timed run(Random rng, int data_size) {
+    return this.test_func(rng, data_size);
+  }
+}
+
+class TestSuite
+() {
+  List<Test> tests = new List<Test>();
+  int repeats = 1;
+  List<int> ds = new List<int>();
+
+  public static TestSuite New() { return new TestSuite(); }
+  public TestSuite add_test(Test t) {
+    this.tests.Add(t);
+    return this;
+  }
+  public TestSuite set_repeats(int r) {
+    this.repeats = r;
+    return this;
+  }
+  public TestSuite data_sizes(int[] sizes) {
+    this.ds = new List<int>(sizes);
+    return this;
+  }
+  public TestResults run() {
+    Random rng = new Random();
+    TestResult[] results = new TestResult[this.tests.Count];
+
+    for (int t = 0; t < this.tests.Count; t++) {
+      long[] times = new long[this.ds.Count];
+      for (int data_size_i = 0; data_size_i < this.ds.Count; data_size_i++) {
+        long avg = 0;
+        for (int r = 0; r < this.repeats; r++) {
+          Console.WriteLine(
+              $"Testing data size {this.ds[data_size_i]} for {this.tests[t].name}: iteration {r+1}");
+          avg += this.tests[t].run(rng, this.ds[data_size_i]).time_taken;
+        }
+        times[data_size_i] = avg / this.repeats;
+      }
+      results[t] = new TestResult(times, this.tests[t].name);
     }
 
-    class Timed
-    {
-        public long millis;
-        public Timed(long time) { this.millis = time; }
-        public static Timed time(Action func) {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            func();
-            return new Timed(watch.ElapsedMilliseconds);
-        }
-    }
-    class Test
-    {
-        string test_name;
-        int repeats;
-        Func<Random, Timed> test_func;
-       
-        public TestResult run()
-        {
-            Random rng = new Random();
-            long avg = 0;
-            for(int i = 0; i < this.repeats; i++)
-            {
-                avg += this.test_func(rng).millis;
-            }
-
-            return new TestResult(this.test_name, this.repeats, avg / this.repeats);
-        }
-
-        public Test(Func<Random, Timed> test_func, string name, int repeats)
-        {
-            this.test_name = name;
-            this.repeats = repeats;
-            this.test_func = test_func;
-        }
-    }
+    return new TestResults(results, this.ds.ToArray());
+  }
+}
 }
